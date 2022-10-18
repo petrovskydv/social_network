81794fd3048f6d56f2229ac8743a5fc1b21cfdb7
petrovskyd <petrovsky.dv@gmail.com>
2022-10-18 17:03:30 +0600

    add user model

--- social_network/settings.py
+++ social_network/settings.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -81,3 +82,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'posts.User'
