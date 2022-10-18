d18149b014d29a4673c1c3d3f3aa3cd60df2bd3c
petrovskyd <petrovsky.dv@gmail.com>
2022-10-18 22:55:28 +0600

    add endpoint for likes

--- posts/urls.py
+++ posts/urls.py
@@ -4,11 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from posts.views import UserSingUp, PostCreate
+from posts.views import UserSingUpView, PostCreateView, LikeView
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('singup/', UserSingUp.as_view(), name='singup'),
-    path('post/', PostCreate.as_view(), name='post'),
+    path('singup/', UserSingUpView.as_view(), name='singup'),
+    path('post/', PostCreateView.as_view(), name='post'),
+    path('post/<int:pk>/like', LikeView.as_view(), name='like'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,20 @@
 from rest_framework import status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from posts.models import User
+from posts.models import User, Post
 from posts.serializers import UserSerializer, PostSerializer
 
 
-class UserSingUp(CreateAPIView):
+class UserSingUpView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class PostCreate(APIView):
+class PostCreateView(APIView):
     serializer_class = PostSerializer
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -26,3 +26,20 @@ class PostCreate(APIView):
             serializer.save(author=self.request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LikeView(APIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        post.likes.add(self.request.user)
+
+        return Response('OK', status=status.HTTP_201_CREATED)
+
+    def delete(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        post.likes.remove(self.request.user)
+
+        return Response('OK', status=status.HTTP_204_NO_CONTENT)
